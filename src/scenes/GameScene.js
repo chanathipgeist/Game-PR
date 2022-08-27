d73cfd60df931618305f97f1969c93485efe74ef
= <=>
2022-08-28 01:10:26 +0700

    Add first Scene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,23 +1,21 @@
-import Phaser from "phaser";
 
 class GameScene extends Phaser.Scene {
-    constructor(test) {
+    constructor() {
         super({
             key: 'GameScene'
         });
     }
 
     preload() {
-
+        
     }
 
     create() {
-
+        this.add.text(20,20,"TEST", { fontSize: "50px"})
 
     }
 
-    update(delta, time) {
+    update() {
 
     }
 }
-export default GameScene;
