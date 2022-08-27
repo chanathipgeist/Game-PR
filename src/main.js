362e42c501714bdabeb7d0573985a37639ee3753
Kira <c3bosskung@gmail.com>
2022-08-27 17:44:39 +0700

    start project

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import 'phaser';
+import Phaser from 'phaser';
+import GameScene from './scenes/GameScene';
+
+
+const config = {
+    // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
+    type: Phaser.WEBGL,
+    pixelArt: true,
+    roundPixels: true,
+    parent: 'content',
+    width: 720,
+    height: 1280,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: [
+        GameScene
+    ],
+
+
+};
+
+const game = new Phaser.Game(config);
---
+++ src/scenes/GameScene.js
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+
+class GameScene extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: 'GameScene'
+        });
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+
+    }
+
+    update(delta, time) {
+
+    }
+}
+export default GameScene;
